baf4fae3ed1ca381893180b7da85da8ea075855e
Vamshi-Jella <vamshijella21@gmail.com>
2024-11-04 16:17:48 +0530

    Added Nav components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
+import Footer from "./Components/Footer";
+import Loader from "./Components/Loader";
+import { Navbar } from "./Components/Navbar";
 
-import React from 'react'
+import AllRoutes from "./Routers/AllRoutes";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+      setIsLoading(true);
+      let timer = setTimeout(() => {
+          setIsLoading(false);
+          return () => clearInterval(timer);
+      }, 1000);
+  }, []);
+    return isLoading?(
+      <Loader/>
+    ):(
+    <>
+      <Navbar/>  
+        <div style={{marginTop : "150px"}}></div>
+        
+        <AllRoutes />
+    
+      
+      <Footer/>
+    </>
+    )
+
+
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+  <ChakraProvider>
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>
+  </ChakraProvider>
+  </Provider>
+);
